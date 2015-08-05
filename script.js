673b0fd752254ed7d3135425801bd1a4a903bec1
cowalla <wallaconno@gmail.com>
2015-08-05 15:28:10 -0700

    clean up api

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-var oUrl = 'https://evening-mountain-7853.herokuapp.com/streams/';
-var localUrl = 'https://localhost:4000/streams/';
-var uUrl = function(user_key){
-    return oUrl + user_key + '/stream_info_user/';
+var oUrl = 'https://evening-mountain-7853.herokuapp.com/streams';
+var localUrl = 'https://localhost:8000/streams/users';
+var uUrl = function(){
+    return oUrl + '/post_stream_info/';
 };
 
 var session = function(i, p) {
-    console.log(i, p);
+    if(!i||!p){
+        return {}
+    }
+
     return {
-        'current_position': parseInt(i[0].getAttribute('aria-valuenow')),
-        'time': Math.floor(Date.now()),
-        'url': p[0].href.split('/').pop()
+        'p': parseInt(i[0].getAttribute('aria-valuenow')),
+        't': Math.floor(Date.now()),
+        'song_title': p[0].href.split('/').pop()
     }
 };
 var postIt = function(infoElt, progressElt) {
-    var context = JSON.stringify(session(infoElt, progressElt));
+    var context = session(infoElt, progressElt);
     console.log(context);
+    if(context){
+        $.ajax(
+            {
+                type: 'POST',
+                url: uUrl(),
+                data: context,
+                success: function(data) {
+                    console.log(data);
+                }
+            }
+        );
+    }
+};
+
+var getToken = function() {
     $.ajax(
         {
-            type: 'POST',
-            url: uUrl(1),
-            data: context,
+            type: 'GET',
+            url: 'https://localhost:8000/streams/token/',
             success: function(data) {
+                window.stored_cookie = JSON.parse(data);
             }
         }
     );
@@ -29,10 +47,10 @@ var postIt = function(infoElt, progressElt) {
 $(document).ready(function(){
     var func = function(){
         var soundProgress = $('.playbackTimeline__progressWrapper');
-        var soundInfo = $('.playbackSoundBadge__title')
+        var soundInfo = $('.playbackSoundBadge__title');
         postIt(soundProgress, soundInfo)
     };
 
-    setInterval(func, 1000);
+    setInterval(func, 4000);
 }
 );
